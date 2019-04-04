df24248c97472b80aa22e67c64722ebca654f786
Shreya Kapoor <shreyakapoor98@gmail.com>
2019-04-05 00:02:53 +0530

    validation css

--- public_static/Login/login.js
+++ public_static/Login/login.js
@@ -1,23 +1,26 @@
-function validate() {
-    if(document.myform.email == " ")
+function loginvalidate() {
+    if(document.forms["myloginform"]["email"] == "")
     {
-        alert("Please provide your email ");
+        //alert("Please provide your email ");
+        document.getElementById("email").style.color = "red";
         document.myform.email.focus();
         return false;
     }
-    if(document.myform.pwd == " ")
+    if(document.forms["myloginform"]["pwd"] == "")
     {
-        alert("Please provide your password ");
+        //alert("Please provide your password ");
+        document.getElementById("pwd").style.borderColor = "red";
         document.myform.pwd.focus();
         return false;
     }
-    var loginemail = document.myform.email.value;
-    loginatpos = email.indexOf("@");
-    logindotpos = email.lastIndexOf(".");
+    var loginemail = document.myloginform.email.value;
+    loginatpos = loginemail.indexOf("@");
+    logindotpos = loginemail.lastIndexOf(".");
     if (loginatpos < 1 || (logindotpos - loginatpos < 2))
     {
-        alert("Please enter correct email ID")
-        document.myform.email.focus();
+        //alert("Please enter correct email ID")
+        document.getElementById("email").style.borderColor = "red";
+        document.myloginform.email.focus();
         return false;
     }
     return true;
--- public_static/Login/signup.js
+++ public_static/Login/signup.js
@@ -1,31 +1,34 @@
 function signupvalidate() {
-    if(document.mysignupform.signupname == " ")
+    if(document.forms["mysignupform"]["signupname"].value == "")
     {
-        alert("Please provide your name ");
+        document.getElementById("signupname").style.borderColor = "red";
         document.mysignupform.signupname.focus();
         return false;
     }
-    if(document.mysignupform.signupemail == " ")
+    if(document.forms["mysignupform"]["signupemail"] == "")
     {
-        alert("Please provide your email ");
+        document.getElementById("signupemail").style.borderColor = "red";
         document.mysignupform.signupemail.focus();
         return false;
     }
-    if(document.mysignupform.signupdept == " ")
+    if(document.forms["mysignupform"]["signupdept"] == "")
     {
-        alert("Please provide your department ");
+        //alert("Please provide your department ");
+        document.getElementById("signupdept").style.borderColor = "red";
         document.mysignupform.signupdept.focus();
         return false;
     }
-    if(document.mysignupform.designation == " ")
+    if(document.forms["mysignupform"]["designation"] == "")
     {
-        alert("Please provide your designation ");
+        //alert("Please provide your designation ");
+        document.getElementById("designation").style.borderColor = "red";
         document.mysignupform.designation.focus();
         return false;
     }
-    if(document.mysignupform.signuppass == " ")
+    if(document.mysignupform.signuppass == "")
     {
         alert("Please provide your password ");
+        //document.getElementById("signuppass").style.borderColor = "red";
         document.mysignupform.signuppass.focus();
         return false;
     }
@@ -34,7 +37,8 @@ function signupvalidate() {
     signupdotpos = signupemail.lastIndexOf(".");
     if (signupatpos < 1 || (signupdotpos - signupatpos < 2))
     {
-        alert("Please enter correct email ID")
+        //alert("Please enter correct email ID")
+        document.getElementById("signupemail").style.borderColor = "red";
         document.mysignupform.signupemail.focus();
         return false;
     }
